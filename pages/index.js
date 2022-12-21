0033a9d1a8261ed8c22791b8831c2b03c7e7792b
Steven Rose <stevan.rose@digital.homeoffice.gov.uk>
2022-12-21 14:42:48 +0000

    Simple changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -11,11 +11,11 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+          Make a started by editing <code>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
@@ -55,7 +55,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
       </footer>
@@ -111,5 +111,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
